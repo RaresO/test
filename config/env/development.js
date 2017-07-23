fed1eda2a32a64485a387d8191643b2bf86724ea
RaresO <ontrares@yahoo.com>
2017-07-23 15:42:58 +0300

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@ var defaultEnvConfig = require('./default');
 module.exports = {
   db: {
      uri: 'mongodb://kingtsunamy:9z80dS8tRMkI48tlBSw1h4jHOWeldaBtcaI6wbMXAoEiDWgtV27YZHd8QTYywYymYzf2GZuPAbC7AEklQX5Jdg==@kingtsunamy.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', //process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
-    options: {
+     options: {
       user: '',
       pass: ''
     },
